035e8192f380a3ed6d97e4401caf1b8a9c96e765
weiss <weiss.personal@gmail.com>
2018-09-04 10:38:03 -0700

    Use snapshot version of sequencer-framework

--- project/Libs.scala
+++ project/Libs.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 object Libs {
-//  private val Org     = "org.tmt"
-//  private val Version = "0.1-SNAPSHOT"
-  private val Org     = "com.github.tmtsoftware.esw-prototype"
-  private val Version = "7d5f564"
+  private val Org     = "org.tmt"
+  private val Version = "0.1.0-SNAPSHOT"
+//  private val Org     = "com.github.tmtsoftware.esw-prototype"
+//  private val Version = "7d5f564"
 
   val `sequencer-framework` = Org %% "sequencer-framework" % Version
   val `scalaTest`           = "org.scalatest" %% "scalatest" % "3.0.4" % Test
