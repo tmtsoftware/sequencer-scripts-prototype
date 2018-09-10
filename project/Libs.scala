291782128348e794d03a3a72b389fcca12d96d12
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-09-10 10:48:07 +0530

    Upgarde to latest esw-sequencer

--- project/Libs.scala
+++ project/Libs.scala
@@ -3,17 +3,10 @@ import sbt._
 object Libs {
 //  private val Org     = "org.tmt"
 //  private val Version = "0.1-SNAPSHOT"
+
   private val Org     = "com.github.tmtsoftware.esw-prototype"
-  private val Version = "1b7e582"
+  private val Version = "b0486c3"
 
   val `sequencer-framework` = Org %% "sequencer-framework" % Version
   val `scalaTest`           = "org.scalatest" %% "scalatest" % "3.0.4" % Test
-}
-
-object Akka {
-  val Version = "2.5.11"
-
-  val `akka-stream`        = "com.typesafe.akka" %% "akka-stream"        % Version
-  val `akka-typed`         = "com.typesafe.akka" %% "akka-actor-typed"   % Version
-  val `akka-typed-testkit` = "com.typesafe.akka" %% "akka-testkit-typed" % Version
 }
