333a0c65a0d425eb09b57c4920964268f1b4f58b
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-09-11 14:04:14 +0530

    Update latest sha from esw-prototype for sequencer-framework
    Co-authored-by: Poorva Gokhale <13188905+Poorva17@users.noreply.github.com>

--- project/Libs.scala
+++ project/Libs.scala
@@ -5,8 +5,8 @@ object Libs {
 //  private val Version = "0.1-SNAPSHOT"
 
   private val Org     = "com.github.tmtsoftware.esw-prototype"
-  private val Version = "b0486c3"
+  private val Version = "9d07236"
 
-  val `sequencer-framework` = Org %% "sequencer-framework" % Version
-  val `scalaTest`           = "org.scalatest" %% "scalatest" % "3.0.4" % Test
+  val `sequencer-framework` = Org             %% "sequencer-framework" % Version
+  val `scalaTest`           = "org.scalatest" %% "scalatest"           % "3.0.4" % Test
 }
