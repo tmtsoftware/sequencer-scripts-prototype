c02bfd817711fc1886e58186420ce650a7718a9c
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-09-11 18:58:03 +0530

    Upgrade sha for esw-prototype
    Co-authored-by: Bharat Singh <10626464+Bharat1103@users.noreply.github.com>

--- project/Libs.scala
+++ project/Libs.scala
@@ -5,7 +5,7 @@ object Libs {
 //  private val Version = "0.1-SNAPSHOT"
 
   private val Org     = "com.github.tmtsoftware.esw-prototype"
-  private val Version = "9d07236"
+  private val Version = "eadcb66"
 
   val `sequencer-framework` = Org             %% "sequencer-framework" % Version
   val `scalaTest`           = "org.scalatest" %% "scalatest"           % "3.0.4" % Test
