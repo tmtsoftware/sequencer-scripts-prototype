90dc46be2a7552e19034ecc6a868394641bbbefe
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-09-17 12:01:33 +0530

    upgrade sequencer-framework dependnecy

--- project/Libs.scala
+++ project/Libs.scala
@@ -5,7 +5,7 @@ object Libs {
 //  private val Version = "0.1-SNAPSHOT"
 
   private val Org     = "com.github.tmtsoftware.esw-prototype"
-  private val Version = "ac28509"
+  private val Version = "3a4c3ce"
 
   val `sequencer-framework` = Org             %% "ocs-framework" % Version
   val `scalaTest`           = "org.scalatest" %% "scalatest"           % "3.0.4" % Test
