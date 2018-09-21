37de87fb52311e859a992ca0f8e79e650ddbcbb8
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-09-21 13:56:32 +0530

    upgrade esw-prototype version

--- project/Libs.scala
+++ project/Libs.scala
@@ -5,7 +5,7 @@ object Libs {
 //  private val Version = "0.1-SNAPSHOT"
 
   private val Org     = "com.github.tmtsoftware.esw-prototype"
-  private val Version = "d5e5294"
+  private val Version = "5c8adb1"
 
   val `ocs-framework` = Org             %% "ocs-framework" % Version
   val `scalaTest`           = "org.scalatest" %% "scalatest"           % "3.0.4" % Test
