aa712c83d464046036146f7adff90fc4fda58219
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-09-25 14:41:55 +0530

    elaborate test significance

--- tests/iris/IrisImagerOnlyTest.scala
+++ tests/iris/IrisImagerOnlyTest.scala
@@ -12,11 +12,18 @@ import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
 class IrisImagerOnlyTest extends FunSuite {
-  test("should be able to create filter command") {
+  test("test handler for command setupObservation") {
     implicit val system: ActorSystem = ActorSystem("test")
     val mockCswServices              = CswServicesMock.create(SequencerFactory.create())
     val irisImagerOnly                = new IrisImagerOnly(mockCswServices)
 
+
+    /*Without setting values for following keys, it will throw run time exception
+    as script does .head to get values script line no - 13 to 15
+    * If submitted sequence contains such command then it will throw
+    * run time exception and then we need to kill sequencer process as sequencer
+    * will be stuck with inflight command
+    * Are we looking to catch these kind of errors from tests?? */
     val filterKeyParamset = KeyType.ChoiceKey.make("filter", filterChoices).set(Choice("H"))
     val itimeKeyParamset = IntKey.make("imagerIntegrationTime").set(5)
     val rampsKeyParamset = IntKey.make("imagerNumRamps").set(5)
