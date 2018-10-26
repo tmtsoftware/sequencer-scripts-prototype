bbb58af6c0e19e2d37c9508e504b16237962d999
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-10-26 13:43:16 +0530

    Add sequencer bootstrap script for generating executable artefacts for a specific version.

---
+++ deploy/scripts/sequencer-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 0 ]
+then
+mkdir -p ../../target/coursier/stage/"$1"/bin
+cp coursier ../../target/coursier/stage/"$1"/bin
+
+./coursier bootstrap -r jitpack com.github.tmtsoftware.sequencer-scripts::sequencer-scripts:$1 -M Main -f -o ../../target/coursier/stage/"$1"/bin/sequencer-app
+
+echo "Artifacts successfully generated"
+else
+echo "[ERROR] Provide version ID as argument"
+fi
