0b644b7e0c17c64e4dec4dc313b516e2d7e0a10b
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2019-02-14 13:32:46 +0530

    #000: Bharat : Add example to show concurrent handling of commands and events

---
+++ scripts/examples/DemoSequencer.scala
@@ -0,0 +1,53 @@
+package examples
+
+import ocs.framework.ScriptImports._
+
+class DemoSequencer(csw: CswServices) extends Script(csw) {
+
+  private var eventCount  = 0
+  private var stopLooping = false
+
+  private val publisherStream = csw.publish(5.seconds) {
+    SystemEvent(Prefix("demo-test"), EventName("system"))
+  }
+
+  private val subscriptionStream = csw.subscribe(Set(EventKey("demo-test.system"))) { eventKey =>
+    eventCount = eventCount + 1
+    println(s"------------------> received-event for demo sequencer on key: $eventKey")
+    Done
+  }
+
+  handleSetupCommand("setup") { command =>
+    println(s"[Demo] Received command: ${command.commandName}")
+
+    spawn {
+      loop(500.millis) {
+        spawn {
+          println("Handling long running command")
+          if (eventCount >= 10) stopLooping = true
+          stopWhen(stopLooping)
+        }
+      }.await
+
+      val response = csw.submit("Sample1Assembly", command).await
+      csw.addOrUpdateCommand(response)
+      Done
+    }
+  }
+
+  handleObserveCommand("observe") { command =>
+    spawn {
+      println(s"[Demo] Received command: ${command.commandName}")
+      val response = csw.submit("Sample1Assembly", command).await
+      csw.addOrUpdateCommand(response)
+      Done
+    }
+  }
+
+  override def onShutdown(): Future[Done] = spawn {
+    println("shutdown ocs")
+    subscriptionStream.unsubscribe().await
+    publisherStream.cancel()
+    Done
+  }
+}
